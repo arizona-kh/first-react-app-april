632491b1c5687d3b4225ea1ff4a24a4d8a6f0ad1
arizona-kh <arizonaold@MacBook-Pro-admin.local>
2020-04-22 17:54:13 +0300

    added final form & text field  validation

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import { 
+  makeStyles,
+  Avatar,
+  Button,
+  CssBaseline,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Container,
+  Typography
+} from '@material-ui/core';
+import { Form, Field } from 'react-final-form';
+import { TextField} from 'final-form-material-ui';
 
 
 
@@ -52,6 +55,36 @@ const useStyles = makeStyles((theme) => ({
 export default function SignUp() {
   const classes = useStyles();
 
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  const onSubmit = async values => {
+    await sleep(300)
+    window.alert(JSON.stringify(values, 0, 2))
+  };
+
+  const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length <= 5) {
+      errors.firstName = 'First Name must be at least 6 charcters long';
+    }
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length <= 5) {
+      errors.lastName = 'Last Name must be at least 6 charcters long';
+    }
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if(!values.email.match( /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)) {
+      errors.email = "Please enter a valid e-mail adress"
+    }
+    if (!values.password) {
+      errors.password = 'Required';
+    }
+    return errors;
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -62,44 +95,55 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <Form 
+          onSubmit={onSubmit}
+          validate={validate}
+          render={({ handleSubmit, submitting, pristine, values }) => (
+          <form 
+          className={classes.form} 
+          onSubmit={handleSubmit}
+          >
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
+              <Field
+                component={TextField}
                 name="firstName"
                 variant="outlined"
                 required
                 fullWidth
                 id="firstName"
                 label="First Name"
+                placeholder="First Name"
                 autoFocus
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
+              <Field
+                component={TextField}
                 variant="outlined"
                 required
                 fullWidth
                 id="lastName"
                 label="Last Name"
+                placeholder="Last Name"
                 name="lastName"
-                autoComplete="lname"
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <Field
+                component={TextField}
                 variant="outlined"
                 required
                 fullWidth
                 id="email"
                 label="Email Address"
+                placeholder="example@mail.com"
                 name="email"
-                autoComplete="email"
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <Field
+                component={TextField}
                 variant="outlined"
                 required
                 fullWidth
@@ -107,7 +151,6 @@ export default function SignUp() {
                 label="Password"
                 type="password"
                 id="password"
-                autoComplete="current-password"
               />
             </Grid>
             <Grid item xs={12}>
@@ -117,23 +160,46 @@ export default function SignUp() {
               />
             </Grid>
           </Grid>
+          <Grid 
+            container 
+            spacing={2}
+            alignItems="center">
+          <Grid item xs={6}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="default"
+            className={classes.submit}
+            href="/"
+          >
+            Back Home
+          </Button>
+          </Grid>
+          <Grid item xs={6}>
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={submitting || pristine}
           >
             Sign Up
           </Button>
+          </Grid>
+          </Grid>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signin" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
           </Grid>
+          <pre>{JSON.stringify(values, 0, 2)}</pre>
         </form>
+          )}
+        />
       </div>
       <Box mt={5}>
         <Copyright />
