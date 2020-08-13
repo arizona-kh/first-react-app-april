64f7a48e6d4066ad5e0c86485ff4273b51bf5ff0
arizona-kh <arizonaold@MacBook-Pro-admin.local>
2020-08-13 11:27:37 +0300

    navbar into functions (replaced classses), not finished drawer styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,97 @@
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
+import { 
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+  useTheme,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Hidden
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import { Link } from 'react-router-dom';
 import { auth } from '../configs/firebase.config';
 import { connect } from 'react-redux';
+
+const drawerWidth = '100vw';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  drawer: {
+    [theme.breakpoints.down('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
   menuButton: {
     marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
   },
-  title: {
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {[theme.breakpoints.up('sm')]: {
+    display: 'none',
+    }
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
   },
+  closeMenuButton: {
+    marginRight: 0,
+    marginLeft: 'auto',
+  }
 }));
+
 
 const NavBar = ({currentUser}) => {
   const classes = useStyles();
+  const dummyCategories = ['Hokusai', 'Hiroshige', 'Utamaro', 'Kuniyoshi', 'Yoshitoshi']
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const theme = useTheme();
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen)
+  }
+
+const drawer = (
+    <div>
+      <List>
+        {dummyCategories.map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton 
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+            >
+          <MenuIcon />
+          </IconButton>
           <Button 
             color="inherit"
             component={Link}
@@ -60,6 +124,40 @@ const NavBar = ({currentUser}) => {
           ) : null }
         </Toolbar>
       </AppBar>
+
+      <nav className={classes.drawer}>
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, 
+            }}
+          >
+            <IconButton onClick={handleDrawerToggle} className={classes.closeMenuButton}>
+              <CloseIcon/>
+            </IconButton>
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.toolbar} />
+            {drawer}
+          </Drawer>  
+        </Hidden>
+      </nav>
     </div>
   );
 };
